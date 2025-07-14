067996b92d81c9f695358e0263da129f4a222aad
Anto Christopher <anto@deepsource.io>
2025-07-14 19:19:22 +0530

    refactor: secure file path resolution to prevent relative path traversal

--- packages/evershop/src/lib/middlewares/publicStatic.js
+++ packages/evershop/src/lib/middlewares/publicStatic.js
@@ -1,5 +1,5 @@
 const fs = require('fs').promises;
-const { join } = require('path');
+const { join, normalize, resolve, sep } = require('path');
 const staticMiddleware = require('serve-static');
 const { CONSTANTS } = require('../helpers');
 
@@ -10,11 +10,19 @@ module.exports = async function publiStatic(request, response, next) {
     if (!path.includes('.')) {
       throw new Error('No file extension');
     }
+    // Normalize and sanitize the requested path to prevent path traversal
+    const normalizedPath = normalize(path);
+    const baseDir = join(CONSTANTS.ROOTPATH, 'public');
+    const resolvedPath = resolve(baseDir, normalizedPath);
+    // Ensure the resolved path is within the public directory
+    if (!resolvedPath.startsWith(resolve(baseDir) + sep)) {
+      throw new Error('Invalid path');
+    }
     // Asynchoronously check if the path is a file and exists in the public folder
-    const test = await fs.stat(join(CONSTANTS.ROOTPATH, 'public', path));
+    const test = await fs.stat(resolvedPath);
     if (test.isFile()) {
       // If it is a file, serve it
-      staticMiddleware(join(CONSTANTS.ROOTPATH, 'public'))(
+      staticMiddleware(baseDir)(
         request,
         response,
         next
