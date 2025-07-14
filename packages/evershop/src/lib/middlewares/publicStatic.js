298e72ce97d0aaa9e784b0e5b24b77fa2a8ed7a7
Anto Christopher <anto@deepsource.io>
2025-07-14 19:27:28 +0530

    refactor: sanitize request paths to prevent relative path traversal

--- packages/evershop/src/lib/middlewares/publicStatic.js
+++ packages/evershop/src/lib/middlewares/publicStatic.js
@@ -1,5 +1,5 @@
 const fs = require('fs').promises;
-const { join } = require('path');
+const { join, normalize, isAbsolute } = require('path');
 const staticMiddleware = require('serve-static');
 const { CONSTANTS } = require('../helpers');
 
@@ -7,11 +7,20 @@ module.exports = async function publiStatic(request, response, next) {
   // Get the request path
   const { path } = request;
   try {
-    if (!path.includes('.')) {
+    // Normalize and validate the requested path to prevent path traversal
+    let requestedPath = path;
+    if (requestedPath.startsWith('/') || requestedPath.startsWith('\\')) {
+      requestedPath = requestedPath.slice(1);
+    }
+    const normalizedPath = normalize(requestedPath);
+    if (normalizedPath.includes('..') || isAbsolute(normalizedPath)) {
+      throw new Error('Invalid path');
+    }
+    if (!normalizedPath.includes('.')) {
       throw new Error('No file extension');
     }
     // Asynchoronously check if the path is a file and exists in the public folder
-    const test = await fs.stat(join(CONSTANTS.ROOTPATH, 'public', path));
+    const test = await fs.stat(join(CONSTANTS.ROOTPATH, 'public', normalizedPath));
     if (test.isFile()) {
       // If it is a file, serve it
       staticMiddleware(join(CONSTANTS.ROOTPATH, 'public'))(
